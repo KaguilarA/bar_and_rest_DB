0de5cffa61eec7ad97f768338a13f9680888f546
Kevin Aguilar <kevin.231@hotmail.com>
2024-11-29 08:35:41 -0600

    Create update-stock-after-insert.sql

---
+++ triggers/update-stock-after-insert.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE TRIGGER UpdateStockAfterInsert
+AFTER INSERT ON products_by_invoice
+FOR EACH ROW
+BEGIN
+    UPDATE products
+    SET stock = stock - NEW.quantity
+    WHERE id = NEW.product_id;
+END$$
+
+DELIMITER ;
