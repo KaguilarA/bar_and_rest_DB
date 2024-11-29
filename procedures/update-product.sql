39368ed0c7c43ed046b64a41f8798abe7983a46e
Kevin Aguilar <kevin.231@hotmail.com>
2024-11-29 08:16:01 -0600

    add update user and update product procedures

---
+++ procedures/update-product.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+
+CREATE PROCEDURE UpdateProduct(
+    IN p_id INT,
+    IN p_name VARCHAR(100),
+    IN p_type INT,
+    IN p_stock INT,
+    IN p_price DECIMAL(10,2)
+)
+BEGIN
+    UPDATE `products`
+    SET `name` = p_name, `type` = p_type, `stock` = p_stock, `price` = p_price
+    WHERE `id` = p_id;
+END$$
+
+DELIMITER ;
---
+++ procedures/update-user.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE PROCEDURE UpdateUser(
+    IN p_user_id INT,
+    IN p_username VARCHAR(100),
+    IN p_email VARCHAR(100)
+)
+BEGIN
+    UPDATE `users`
+    SET `username` = p_username, `email` = p_email
+    WHERE `id` = p_user_id;
+END$$
+
+DELIMITER ;
