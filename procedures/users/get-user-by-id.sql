6ebbaf4d2b21d357d75fb194018f37443ebcdb23
Kevin Aguilar <kevin.231@hotmail.com>
2024-11-30 05:22:26 -0600

    refactor files

--- procedures/users/get-user-by-id.sql
+++ procedures/users/get-user-by-id.sql
@@ -4,7 +4,9 @@ DROP PROCEDURE IF EXISTS GetUserById$$
 
 CREATE PROCEDURE GetUserById(IN p_id INT)
 BEGIN
-    SELECT `id`, `name`, `lastname`, `username`, `state`, `date_created` WHERE `id` = p_id;
+    SELECT `id`, `name`, `lastname`, `username`, `state`, `date_created`
+    FROM `users`
+    WHERE `id` = p_id;
 END$$
 
 DELIMITER ;
