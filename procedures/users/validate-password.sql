86f8914c7e23c1a0babd58b522e5ae8334a89728
Kevin Aguilar <kevin.231@hotmail.com>
2024-11-30 06:55:08 -0600

    Update validate-password.sql

--- procedures/users/validate-password.sql
+++ procedures/users/validate-password.sql
@@ -4,21 +4,23 @@ DROP PROCEDURE IF EXISTS ValidatePasswordHash$$
 
 CREATE PROCEDURE ValidatePasswordHash(
     IN p_username VARCHAR(50),
-    IN p_password VARCHAR(255),
-    OUT is_valid BOOLEAN
+    IN p_password VARCHAR(255)
 )
 BEGIN
     DECLARE hashed_password VARCHAR(255);
+    DECLARE is_valid BOOLEAN;
 
-    SELECT `password` INTO hashed_password
+    SELECT password_hash INTO hashed_password
     FROM users
-    WHERE username = p_username;
+    WHERE username = p_username COLLATE utf8mb4_unicode_ci;
 
-    IF hashed_password IS NOT NULL AND hashed_password = SHA2(p_password, 256) THEN
+    IF hashed_password IS NOT NULL AND hashed_password = SHA2(p_password COLLATE utf8mb4_unicode_ci, 256) THEN
         SET is_valid = TRUE;
     ELSE
         SET is_valid = FALSE;
     END IF;
+
+    SELECT is_valid AS is_valid;
 END$$
 
 DELIMITER ;
