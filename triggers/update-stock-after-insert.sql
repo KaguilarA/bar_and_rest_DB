8e91c6b6bbfd2d47e97c42d3a86aef7a44a565ca
Kevin Aguilar <kevin.231@hotmail.com>
2024-12-03 03:24:21 -0600

    add documentation for triggers

--- triggers/update-stock-after-insert.sql
+++ triggers/update-stock-after-insert.sql
@@ -1,11 +1,14 @@
 DELIMITER $$
 
+-- Drop the trigger if it already exists
 DROP TRIGGER IF EXISTS UpdateStockAfterInsert$$
 
+-- Create a new trigger that updates the stock after an insert on products_by_invoice
 CREATE TRIGGER UpdateStockAfterInsert
 AFTER INSERT ON products_by_invoice
 FOR EACH ROW
 BEGIN
+    -- Update the stock in the products table based on the inserted quantity
     UPDATE products
     SET stock = stock - NEW.quantity
     WHERE id = NEW.product_id;
--- triggers/update-stock-after-update.sql
+++ triggers/update-stock-after-update.sql
@@ -1,15 +1,20 @@
 DELIMITER $$
 
+-- Drop the trigger if it already exists
 DROP TRIGGER IF EXISTS UpdateStockAfterUpdate$$
 
+-- Create a new trigger that updates the stock after an update on products_by_invoice
 CREATE TRIGGER UpdateStockAfterUpdate
 AFTER UPDATE ON products_by_invoice
 FOR EACH ROW
 BEGIN
+    -- Declare a variable to store the difference in stock
     DECLARE stock_difference INT;
 
+    -- Calculate the difference in quantity between the new and old values
     SET stock_difference = NEW.quantity - OLD.quantity;
 
+    -- Update the stock in the products table based on the stock difference
     UPDATE products
     SET stock = stock - stock_difference
     WHERE id = NEW.product_id;
