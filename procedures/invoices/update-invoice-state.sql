d90da35f76715138ca1089e40e1868be65ed28d3
Kevin Aguilar <kevin.231@hotmail.com>
2024-12-19 12:45:20 -0600

    Update update-invoice-state.sql

--- procedures/invoices/update-invoice-state.sql
+++ procedures/invoices/update-invoice-state.sql
@@ -6,13 +6,26 @@ DROP PROCEDURE IF EXISTS UpdateInvoiceState$$
 -- Create a new procedure to update the state of an invoice by ID
 CREATE PROCEDURE UpdateInvoiceState(
     IN p_invoice_id INT,
-    IN p_state ENUM('pendiente', 'cancelada', 'anulada')
+    IN p_state_id INT
 )
 BEGIN
-    -- Update the state of the invoice in the invoices table
-    UPDATE `invoices`
-    SET `state` = p_state
-    WHERE `id` = p_invoice_id;
+    DECLARE state_exists INT;
+
+    -- Check if the state ID exists in the states table
+    SELECT COUNT(*) INTO state_exists
+    FROM `states`
+    WHERE `id` = p_state_id;
+
+    -- If the state ID does not exist, signal an error
+    IF state_exists = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Invalid state ID';
+    ELSE
+        -- Update the state of the invoice in the invoices table
+        UPDATE `invoices`
+        SET `state_id` = p_state_id
+        WHERE `id` = p_invoice_id;
+    END IF;
 END$$
 
 DELIMITER ;
