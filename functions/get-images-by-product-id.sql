6ba502f3c0762a4bc08fd04f2f420a3620bc6057
Kevin Aguilar <kevin.231@hotmail.com>
2025-03-04 04:00:11 -0600

    Create get-images-by-product-id.sql

---
+++ functions/get-images-by-product-id.sql
@@ -0,0 +1,26 @@
+DELIMITER $$
+
+-- Drop the function if it already exists
+DROP FUNCTION IF EXISTS GetImagesByProductId$$
+
+-- Create a new function to get images by product ID
+CREATE FUNCTION GetImagesByProductId(p_product_id INT)
+RETURNS JSON
+BEGIN
+    DECLARE images_json JSON;
+
+    -- Select images associated with the product and aggregate them into a JSON array
+    SELECT JSON_ARRAYAGG(
+        JSON_OBJECT(
+            'id', pi.id,
+            'image_id', pi.image_id,
+            'is_main', pi.is_main
+        )
+    ) INTO images_json
+    FROM product_images pi
+    WHERE pi.product_id = p_product_id;
+
+    RETURN images_json;
+END$$
+
+DELIMITER ;
